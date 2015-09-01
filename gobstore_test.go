c8c4b515ca66030ed836426636d652d7d36c9407
Jason Croucher <jace.croucher@gmail.com>
2015-08-31 18:26:51 -0700

    updated readme and datastruct

--- gobstore_test.go
+++ gobstore_test.go
@@ -2,20 +2,22 @@ package gobstore
 
 import (
 	"fmt"
-	"github.com/jacec/gobstore/datatype"
 	"testing"
+
+	"github.com/jacec/gobstore/datastruct"
 )
 
 func TestSave(t *testing.T) {
 
-	var dt datatype.DataType
-	var p datatype.Person
-	p.Firstname = "jason"
-	p.Lastname = "croucher"
-	p.Title = "mr"
-	dt.Data = p
-	g := NewGobstore("person")
-	g.Save(dt)
+	var dv datastruct.DataValue
+	var p datastruct.Person
+	p.Firstname = "test-firstname"
+	p.Lastname = "test-lastname"
+	p.Title = "test-title"
+	p.Age = 42
+	dv.Data = p
+	gs := NewGobstore("person")
+	gs.Save(dv)
 
 }
 
